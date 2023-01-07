857082839f925eb021cbe561181daee1263ab602
usamaawan696 <usamaawan57552@gmail.com>
2023-01-07 10:30:53 +0500

    c

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Slide from './Slider';
 // import ControlledCarousel from './Controlslide';
-import NavScrollExample from './Nav';
 
 
 function App() {
